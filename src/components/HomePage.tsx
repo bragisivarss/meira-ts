2b4caff91101c87aca936863d3c6f077e988c1c2
bragisivarss <sigursteinnbragi@gmail.com>
2024-04-12 20:06:11 +0000

    woop woop

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,36 +1,46 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
-import Expense from "@/types/ExpenseType"
-import MapExpense from "@/components/RenderExpense"
+import Expense from "@/types/ExpenseType";
+import MapExpense from "@/components/RenderExpense";
 
 const getExpenseData = async (): Promise<Expense[]> => {
-  const res = await fetch("http://localhost:3001/api/expenses")
+    const res = await fetch("http://localhost:3001/api/expenses");
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
+    }
 
-  const response = await res.json()
-  return response
-}
+    const response = await res.json();
+    return response;
+};
 
 const ExpenseComponent = () => {
-const [data, setData] = useState<Expense[] | null>(null);
+    const [data, setData] = useState<Expense[] | null>(null);
 
     const fetchData = async () => {
-        const fetchedData = await getExpenseData()
-        setData(fetchedData)
-    }
+        const fetchedData = await getExpenseData();
+        setData(fetchedData);
+    };
     useEffect(() => {
-        fetchData()
-    })
+        fetchData();
+    }, []);
 
     if (!data) {
-        return <p>Please Wait Loading..</p>
+        return <p>Please Wait Loading..</p>;
     }
 
-    return (
-    <MapExpense data={data}/>
-    )
-}
+    const removeOne = (id: number) => {
+        const newData = data.filter((data) => data.id != id);
+        setData(newData);
+    };
+
+    const removeAll = () => {
+        setData(null)
+    }
+
+    return <>
+    <MapExpense data={data} removeOne={removeOne} />
+        <button type="button" onClick={removeAll} className="m-10 border border-cyan-600 px-2 py-1">Delete All!</button>
+    </>
+};
 export default ExpenseComponent;
--- src/components/RenderExpense.tsx
+++ src/components/RenderExpense.tsx
@@ -2,15 +2,18 @@ import Expense from "@/types/ExpenseType";
 
 type ExpenseComponentProps = {
   data: Expense[];
+  removeOne: (id: number) => void;
 };
 
-const MapExpense = ({ data }: ExpenseComponentProps) => {
+
+const MapExpense = ({ data, removeOne }: ExpenseComponentProps) => {
   return (
     <>
       {data.map(({ id, name, cost }) => (
-        <div key={id} className="border border-sky-500 mb-4 w-1/4">
+        <div key={id} className="border border-sky-500 p-2 w-44 m-2">
           <h1>{name}</h1>
           <div>{cost}</div>
+          <button type="button" onClick={() => removeOne(id)} className="border border-cyan-300 my-2 py-1 px-2">Delete</button>
         </div>
       ))}
     </>
